38aaa530bb060c0f26e2dc119a66ecf782c1a037
NathanFallet <contact@nathanfallet.me>
2025-06-08 23:47:09 +0200

    kotlin 2.1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    kotlin("android").version("2.0.0").apply(false)
-    kotlin("multiplatform").version("2.0.0").apply(false)
-    kotlin("plugin.serialization").version("2.0.0").apply(false)
-    kotlin("plugin.compose").version("2.0.0").apply(false)
+    kotlin("android").version("2.1.21").apply(false)
+    kotlin("multiplatform").version("2.1.21").apply(false)
+    kotlin("plugin.serialization").version("2.1.21").apply(false)
+    kotlin("plugin.compose").version("2.1.21").apply(false)
     id("com.android.application").version("8.2.1").apply(false)
     id("com.android.library").version("8.2.1").apply(false)
     id("org.jetbrains.kotlinx.kover").version("0.8.0")
-    id("com.google.devtools.ksp").version("2.0.0-1.0.21").apply(false)
+    id("com.google.devtools.ksp").version("2.1.21-2.0.1").apply(false)
     id("app.cash.sqldelight").version("2.0.0").apply(false)
     id("com.rickclephas.kmp.nativecoroutines").version("1.0.0-ALPHA-37").apply(false)
 }
